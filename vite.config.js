db1466a9c315cbf84e3b6713438f31e782f8402a
Prasad Neroolkar <prasadneroolkar90@gmail.com>
2024-07-01 20:53:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Contact-App", // Replace with your repository name
+  base: "/Contact-App/", // Replace with your repository name
 });
