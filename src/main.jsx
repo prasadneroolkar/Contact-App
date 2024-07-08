315946a1de60230905b11b74734fae5b4db7804c
prasad <prasadneroolkar90@gmail.com>
2024-07-08 20:01:48 +0530

    Updated filess

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Contact-App/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
